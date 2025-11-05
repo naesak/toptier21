d23b48b6ddf46c36dd4623fb5e5fd607680c633b
naesak <naielipesakalis@gmail.com>
2025-11-05 00:36:01 -0300

    Create songs.js

---
+++ songs.js
@@ -0,0 +1,342 @@
+var metadata = {
+  HeavyDirtySoul: {
+    album: "Blurryface",
+    trackNumber: 1,
+    image: "img/blurryface.png",
+  },
+  "Stressed Out": {
+    album: "Blurryface",
+    trackNumber: 2,
+    image: "img/blurryface.png",
+  },
+  Ride: {
+    album: "Blurryface",
+    trackNumber: 3,
+    image: "img/blurryface.png",
+  },
+  "Fairly Local": {
+    album: "Blurryface",
+    trackNumber: 4,
+    image: "img/blurryface.png",
+  },
+  "Tear In My Heart": {
+    album: "Blurryface",
+    trackNumber: 5,
+    image: "img/blurryface.png",
+  },
+  "Lane Boy": {
+    album: "Blurryface",
+    trackNumber: 6,
+    image: "img/blurryface.png",
+  },
+  "The Judge": {
+    album: "Blurryface",
+    trackNumber: 7,
+    image: "img/blurryface.png",
+  },
+  Doubt: {
+    album: "Blurryface",
+    trackNumber: 8,
+    image: "img/blurryface.png",
+  },
+  Polarize: {
+    album: "Blurryface",
+    trackNumber: 9,
+    image: "img/blurryface.png",
+  },
+  "We Don't Believe What's On T.V.": {
+    album: "Blurryface",
+    trackNumber: 10,
+    image: "img/blurryface.png",
+  },
+  "Message Man": {
+    album: "Blurryface",
+    trackNumber: 11,
+    image: "img/blurryface.png",
+  },
+  Hometown: {
+    album: "Blurryface",
+    trackNumber: 12,
+    image: "img/blurryface.png",
+  },
+  "Not Today": {
+    album: "Blurryface",
+    trackNumber: 13,
+    image: "img/blurryface.png",
+  },
+  Goner: {
+    album: "Blurryface",
+    trackNumber: 14,
+    image: "img/blurryface.png",
+  },
+  Jumpsuit: {
+    album: "Trench",
+    trackNumber: 1,
+    image: "img/trench.png",
+  },
+  Levitate: {
+    album: "Trench",
+    trackNumber: 2,
+    image: "img/trench.png",
+  },
+  Morph: {
+    album: "Trench",
+    trackNumber: 3,
+    image: "img/trench.png",
+  },
+  "My Blood": {
+    album: "Trench",
+    trackNumber: 4,
+    image: "img/trench.png",
+  },
+  Chlorine: {
+    album: "Trench",
+    trackNumber: 5,
+    image: "img/trench.png",
+  },
+  Smithereens: {
+    album: "Trench",
+    trackNumber: 6,
+    image: "img/trench.png",
+  },
+  "Neon Gravestones": {
+    album: "Trench",
+    trackNumber: 7,
+    image: "img/trench.png",
+  },
+  "The Hype": {
+    album: "Trench",
+    trackNumber: 8,
+    image: "img/trench.png",
+  },
+  "Nico And The Niners": {
+    album: "Trench",
+    trackNumber: 9,
+    image: "img/trench.png",
+  },
+  "Cut My Lip": {
+    album: "Trench",
+    trackNumber: 10,
+    image: "img/trench.png",
+  },
+  Bandito: {
+    album: "Trench",
+    trackNumber: 11,
+    image: "img/trench.png",
+  },
+  "Pet Cheetah": {
+    album: "Trench",
+    trackNumber: 12,
+    image: "img/trench.png",
+  },
+  Legend: {
+    album: "Trench",
+    trackNumber: 13,
+    image: "img/trench.png",
+  },
+  "Leave The City": {
+    album: "Trench",
+    trackNumber: 14,
+    image: "img/trench.png",
+  },
+  "Good Day": {
+    album: "Scaled and Icy",
+    trackNumber: 1,
+    image: "img/sai.png",
+  },
+  Choker: {
+    album: "Scaled and Icy",
+    trackNumber: 2,
+    image: "img/sai.png",
+  },
+  "Shy Away": {
+    album: "Scaled and Icy",
+    trackNumber: 3,
+    image: "img/sai.png",
+  },
+  "The Outside": {
+    album: "Scaled and Icy",
+    trackNumber: 4,
+    image: "img/sai.png",
+  },
+  Saturday: {
+    album: "Scaled and Icy",
+    trackNumber: 5,
+    image: "img/sai.png",
+  },
+  "Never Take It": {
+    album: "Scaled and Icy",
+    trackNumber: 6,
+    image: "img/sai.png",
+  },
+  "Mulberry Street": {
+    album: "Scaled and Icy",
+    trackNumber: 7,
+    image: "img/sai.png",
+  },
+  Formidable: {
+    album: "Scaled and Icy",
+    trackNumber: 8,
+    image: "img/sai.png",
+  },
+  "Bounce Man": {
+    album: "Scaled and Icy",
+    trackNumber: 9,
+    image: "img/sai.png",
+  },
+  "No Chances": {
+    album: "Scaled and Icy",
+    trackNumber: 10,
+    image: "img/sai.png",
+  },
+  Redecorate: {
+    album: "Scaled and Icy",
+    trackNumber: 11,
+    image: "img/sai.png",
+  },
+  Overcompensate: {
+    album: "Clancy",
+    trackNumber: 1,
+    image: "img/clancy.png",
+  },
+  "Next Semester": {
+    album: "Clancy",
+    trackNumber: 2,
+    image: "img/clancy.png",
+  },
+  Backslide: {
+    album: "Clancy",
+    trackNumber: 3,
+    image: "img/clancy.png",
+  },
+  "Midwest Indigo": {
+    album: "Clancy",
+    trackNumber: 4,
+    image: "img/clancy.png",
+  },
+  "Routines In The Night": {
+    album: "Clancy",
+    trackNumber: 5,
+    image: "img/clancy.png",
+  },
+  Vignette: {
+    album: "Clancy",
+    trackNumber: 6,
+    image: "img/clancy.png",
+  },
+  "The Craving (Jenna's Version)": {
+    album: "Clancy",
+    trackNumber: 7,
+    image: "img/clancy.png",
+  },
+  Lavish: {
+    album: "Clancy",
+    trackNumber: 8,
+    image: "img/clancy.png",
+  },
+  Navigating: {
+    album: "Clancy",
+    trackNumber: 9,
+    image: "img/clancy.png",
+  },
+  "Snap Back": {
+    album: "Clancy",
+    trackNumber: 10,
+    image: "img/clancy.png",
+  },
+  "Oldies Station": {
+    album: "Clancy",
+    trackNumber: 11,
+    image: "img/clancy.png",
+  },
+  "At The Risk Of Feeling Dumb": {
+    album: "Clancy",
+    trackNumber: 12,
+    image: "img/clancy.png",
+  },
+  "Paladin Strait": {
+    album: "Clancy",
+    trackNumber: 13,
+    image: "img/clancy.png",
+  },
+  "City Walls": {
+    album: "Breach",
+    trackNumber: 1,
+    image: "img/breach.png",
+  },
+  RAWFEAR: {
+    album: "Breach",
+    trackNumber: 2,
+    image: "img/breach.png",
+  },
+  "Drum Show": {
+    album: "Breach",
+    trackNumber: 3,
+    image: "img/breach.png",
+  },
+  Garbage: {
+    album: "Breach",
+    trackNumber: 4,
+    image: "img/breach.png",
+  },
+  "The Contract": {
+    album: "Breach",
+    trackNumber: 5,
+    image: "img/breach.png",
+  },
+  Downstairs: {
+    album: "Breach",
+    trackNumber: 6,
+    image: "img/breach.png",
+  },
+  "Robot Voices": {
+    album: "Breach",
+    trackNumber: 7,
+    image: "img/breach.png",
+  },
+  "Center Mass": {
+    album: "Breach",
+    trackNumber: 8,
+    image: "img/breach.png",
+  },
+  Cottonwood: {
+    album: "Breach",
+    trackNumber: 9,
+    image: "img/breach.png",
+  },
+  "One Way": {
+    album: "Breach",
+    trackNumber: 10,
+    image: "img/breach.png",
+  },
+  "Days Lie Dormant": {
+    album: "Breach",
+    trackNumber: 11,
+    image: "img/breach.png",
+  },
+  Tally: {
+    album: "Breach",
+    trackNumber: 12,
+    image: "img/breach.png",
+  },
+  Intentions: {
+    album: "Breach",
+    trackNumber: 13,
+    image: "img/breach.png",
+  },
+  Heathens: {
+    album: "Single",
+    trackNumber: "Single",
+    image: "img/heathens.png",
+  },
+  "The Craving (Single Version)": {
+    album: "Single",
+    trackNumber: "Single",
+    image: "img/craving.png",
+  },
+  "The Line": {
+    album: "Single",
+    trackNumber: "Single",
+    image: "img/line.png",
+  },
+};
